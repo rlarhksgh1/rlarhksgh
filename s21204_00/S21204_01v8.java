c71c7db263e26009bdc9bbfd93e2e47dd63b3b2e
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-05-08 12:27:59 +0900

    Create S21204_01v8.java

---
+++ s21204_00/S21204_01v8.java
@@ -0,0 +1,21 @@
+package s21204_00;
+
+public class S21204_01v8 {
+
+	public static void main(String[] args) {
+		
+		int intVal1 = 100;
+		int intVal2 = -100;
+		double douVal1 = 3.14;
+		double douVal2 = -3.14;
+		
+		int intResult1 = -intVal1;
+		int intResult2 = -intVal2;
+		double douResult1 = -douVal1;
+		double douResult2 = -douVal2;
+		
+		System.out.println("int1: " + intResult1+ " int2: "+intResult2);
+	    System.out.println("dou1: " + douResult1+ " dou2: "+douResult2);
+	}
+
+}
