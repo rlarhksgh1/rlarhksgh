905b4ccc431d43d1a9d5348f378a9d93323dab12
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-05-08 12:34:18 +0900

    Create S21204_03.java

---
+++ s21204_00/S21204_03.java
@@ -0,0 +1,25 @@
+package s21204_00;
+
+public class S21204_03 {
+
+	public static void main(String[] args) {
+		
+		int input = 0;
+		
+		if (input == 0) {
+			System.out.println("a : 값은 0 입니다");
+		}
+		if (input != 0) {
+			System.out.println("a : 값은 0이 아닙니다");
+		}
+		
+		if (input == 0) {
+	         System.out.println("b : 값은 0 입니다");
+	         
+	    } 	else {
+	         System.out.println("b : 값은 0 입니다");
+	    	}
+
+	}
+
+}
