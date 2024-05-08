20535c79159ae3a0a9f48ef746279c4a509ef302
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-05-08 12:06:50 +0900

    Create S21204_00.java

---
+++ s21204_00/S21204_00.java
@@ -0,0 +1,22 @@
+package s21204_00;
+
+public class S21204_00 {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		for(int cnt = 1; cnt < 11; cnt ++) {
+			sum = sum + cnt;
+		}	
+			
+		System.out.println("sum : " + sum);
+
+	}
+
+}    
+ 
+ 
+ 
+ 
+ 
+ 
