e34a1bfc59e8bdfb197d751ee6cd725575a8e550
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-05-08 12:09:08 +0900

    Create S21204_01.java

---
+++ s21204_00/S21204_01.java
@@ -0,0 +1,18 @@
+package s21204_00;
+
+public class S21204_01 {
+
+	public static void main(String[] args) {
+
+		int intNum1 = 0;
+		if (intNum1 == 0) {
+			
+			int intNum2 = 10;
+			
+			System.out.println("Num1:" + intNum1 + "  if Num2:" + intNum2);
+			
+		}
+
+	}
+
+}
