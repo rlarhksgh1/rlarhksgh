1c119f89f156f9e5b7183dd5b1feff397269bdec
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-05-08 12:36:30 +0900

    Create S21204_03v41.java

---
+++ s21204_00/S21204_03v41.java
@@ -0,0 +1,18 @@
+package s21204_00;
+
+public class S21204_03v41 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		int i   = 0;
+		while(true) {
+			++i;
+ 			sum += i;
+ 			if(sum > 100) break;
+				System.out.println("i=" +i+ "sum=" +sum);
+		}
+		System.out.println("-----------------");
+		System.out.println("i=" +i+ "sum=" +sum);
+	}
+
+}
