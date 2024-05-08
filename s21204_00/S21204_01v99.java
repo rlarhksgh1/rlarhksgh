f78f7c4db5ff20511ecbc5f6243b9a39a5f7513d
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-05-08 12:32:34 +0900

    Create S21204_01v99.java

---
+++ s21204_00/S21204_01v99.java
@@ -0,0 +1,19 @@
+package s21204_00;
+
+public class S21204_01v99 {
+
+	public static void main(String[] args) {
+		String str1 = "abc";
+		String str2 = "abc";
+		System.out.println("기본변수== :" +(str1==str2));
+		System.out.println("기본변수equ :"+(str1.equals(str2)));
+		
+		String str3 = new String("abc");
+		String str4 = new String("abc");
+		System.out.println("참조변수== :" +(str3==str4));
+		System.out.println("참조변수equ :"+str3.equals(str4));
+
+
+	}
+
+}
