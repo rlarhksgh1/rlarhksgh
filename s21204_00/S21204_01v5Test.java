3186ab19d1c452df4ea36f7bef2bcac3b3679405
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-05-08 12:11:52 +0900

    Create S21204_01v5Test.java

---
+++ s21204_00/S21204_01v5Test.java
@@ -0,0 +1,24 @@
+package s21204_00;
+
+import java.io.IOException;
+
+public class S21204_01v5Test {
+
+	public static void main(String[] args) throws Exception {
+		
+		System.out.println("키코드 입력");
+		
+		int KeyCode = System.in.read();
+		System.out.println("KeyCode: " + KeyCode);
+		
+		int keyCode = System.in.read();
+		System.out.println("KeyCode: " + keyCode);
+		
+		int KEyCode = System.in.read();
+		System.out.println("KeyCode: " + KEyCode);
+		
+	
+
+	}
+
+}
