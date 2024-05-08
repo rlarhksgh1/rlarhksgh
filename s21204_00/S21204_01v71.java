708badb1dfba29c515ef02cb6cebab2a0fea619f
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-05-08 12:26:46 +0900

    Create S21204_01v71.java

---
+++ s21204_00/S21204_01v71.java
@@ -0,0 +1,25 @@
+package s21204_00;
+
+import java.util.Scanner;
+
+public class S21204_01v71 {
+
+	public static void main(String[] args) {
+		
+		String inString;
+		Scanner scanner = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("입력하세요: ");
+			inString = scanner.nextLine();
+			
+			System.out.println("입력된 문자열은:\"" + inString + "\" ");
+			if(inString.equals("q")) {
+				break;
+			}
+		}
+		scanner.close();
+		System.out.println("종료");
+	}
+
+}
