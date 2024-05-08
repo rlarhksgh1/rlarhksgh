8b517c7e3f3324befc4f5bcf5fbaf9dea7ba5223
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-05-08 12:10:12 +0900

    Create S21204_01v3.java

---
+++ s21204_00/S21204_01v3.java
@@ -0,0 +1,18 @@
+package s21204_00;
+
+public class S21204_01v3 {
+
+	public static void main(String[] args) {
+		
+		//boolean stop = true;
+		boolean stop = false;
+		int i = 10;
+				
+		if (i > 9) {
+			System.out.println("운행을 중지합니다");
+		} else {
+			System.out.println("운행을 시작합니다");
+		}
+	}
+
+}
