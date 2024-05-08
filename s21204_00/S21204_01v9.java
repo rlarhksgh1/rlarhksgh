15a8cc36a2494833285a06e25e564d745ea8014b
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-05-08 12:29:59 +0900

    Create S21204_01v9.java

---
+++ s21204_00/S21204_01v9.java
@@ -0,0 +1,18 @@
+package s21204_00;
+
+public class S21204_01v9 {
+
+	public static void main(String[] args) {
+		int intX = 1;
+		int intY = 1;
+		int intXnum = ++intX + 10;
+		int intYnum = intY++ + 10;
+		
+		System.out.println("++intXnum: " +intXnum+" intX: " +intX);
+		System.out.println("intYnum++: " +intYnum+" intY: " +intY);
+		
+
+
+	}
+
+}
