9ab9d65d458d7764935d236b82c1eaa93335b440
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-05-09 11:09:32 +0900

    Create S21204_04.java

---
+++ s21204_00/S21204_04.java
@@ -0,0 +1,14 @@
+package s21204_00;
+
+public class S21204_04 {
+
+	public static void main(String[] args) {
+
+		System.out.println("매개변수의 개수:" +args.length);
+		
+		for (int i=0; i<args.length; i++) {
+			System.out.println("args[" +i+ "] = \"" + args[i] + "\" ");
+		}
+	}
+
+}
