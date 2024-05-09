f3611a50d7ad532ca05c05f1eb9db3081467e4df
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-05-09 11:26:05 +0900

    Create Test01.java

---
+++ s21204_00/Test01.java
@@ -0,0 +1,25 @@
+package s21204_00;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		
+		int intValue = 10;
+		byte byteValue = (byte) intValue;
+				
+		int intvalue = 65;
+		char charValue = (char) intvalue;
+		System.out.println("charValue :" + charValue);
+		
+		double doublevalue = 3.14;
+		int intvAlue = (int) doublevalue;
+		System.out.println("intvAlue : " + intvAlue);
+		
+		int intvaLue = 10;
+		double douValue = 5.7;
+		int result = intvaLue + (int) douValue;
+		System.out.println("result : " + result);
+		
+	}
+
+}
