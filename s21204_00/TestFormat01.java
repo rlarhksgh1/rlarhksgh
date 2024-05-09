cae36fbee7efacc33bf43752020116dba8de0633
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-05-09 11:27:46 +0900

    Create TestFormat01.java

---
+++ s21204_00/TestFormat01.java
@@ -0,0 +1,18 @@
+package s21204_00;
+
+public class TestFormat01 {
+
+	public static void main(String[] args) {
+		
+		System.out.printf("이름: %s", "우리들	");
+		
+		
+		System.out.printf("학번: %d\n", 21204);
+		
+		
+		System.out.println();
+		
+
+	}
+
+}
