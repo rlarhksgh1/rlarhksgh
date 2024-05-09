96868ecbec6c30c41b5edf14fe3c45f1c99b3d1d
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-05-09 11:26:19 +0900

    Create Test02.java

---
+++ s21204_00/Test02.java
@@ -0,0 +1,21 @@
+package s21204_00;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		
+		 int value = 3+7;
+		   System.out.println(value);
+		   
+		   String str1= "3"+7;
+		   System.out.println(str1);
+		   
+		   String str2= 3+"7";
+		   System.out.println(str2);
+		   
+		   System.out.println(value+str1);
+		         
+		
+	}
+
+}
