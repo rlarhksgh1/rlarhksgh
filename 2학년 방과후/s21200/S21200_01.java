acd878f946dc3c2b2efab4d5b19c76a78182ebc9
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-06-26 18:26:51 +0900

    Create S21200_01.java

---
+++ 2학년 방과후/s21200/S21200_01.java
@@ -0,0 +1,15 @@
+package s21200;
+
+public class S21200_01 {
+
+	public static void main(String[] args) {
+		int intNum1 = 0;
+		if (intNum1 == 0) {
+			int intNum2 = 10;
+			System.out.println("Num1: " + intNum1 + " if Num2: " + intNum2);		
+		}
+		int intNum2 = 0;
+		System.out.println("Num1: " + intNum1 + " if Num2: " + intNum2);
+	}
+
+}
