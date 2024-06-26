c391283f33e9eedc827c040e4be7f7e553fd3118
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-06-26 18:31:40 +0900

    Create S21200_01v4.java

---
+++ 2학년 방과후/s21200/S21200_01v4.java
@@ -0,0 +1,18 @@
+package s21200;
+
+public class S21200_01v4 {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		double result = x/y;
+		
+		System.out.println("result_0: " + result);
+		System.out.println("result_1: " + (double) x/y);
+		System.out.println("result_2: " + x / (double) y);
+		System.out.println("result_3: " + (double) x / (double) y);
+		
+
+	}
+
+}
