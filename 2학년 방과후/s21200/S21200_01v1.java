f880ab725843af4c7225272aeb6c302ebaf074d0
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-06-26 18:27:12 +0900

    Create S21200_01v1.java

---
+++ 2학년 방과후/s21200/S21200_01v1.java
@@ -0,0 +1,19 @@
+package s21200;
+
+public class S21200_01v1 {
+
+	public static void main(String[] args) {
+		int intNum1 = 0b1011;
+		int intNum2 = 0206;
+		int intNum3 = 365;
+		int intNum4 = 0xB3;
+		
+		System.out.println("Num1:"+intNum1);
+		System.out.println("Num2:"+intNum2);
+		System.out.println("Num3:"+intNum3);
+		System.out.println("Num4:"+intNum4);
+		
+
+	}
+
+}
