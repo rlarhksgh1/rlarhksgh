ad8bfb867bf4f5d00e1c70962b63de61f1b99ed9
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-06-27 10:42:46 +0900

    Create Resursion4_Fibonachi.java

---
+++ s21204_recursion/Resursion4_Fibonachi.java
@@ -0,0 +1,18 @@
+package s21204_recursion;
+
+public class Resursion4_Fibonachi {
+
+   static int fibonachi(int num) {
+      System.out.println("fibonachi : " + num);
+      if (num < 2) {
+         return num;
+      } else {
+         System.out.println("fibonachi if : " + num);
+         return fibonachi(num - 1) + fibonachi(num - 2);
+      }
+   }
+
+   public static void main(String[] args) {
+      System.out.println("fibonachi number : " + fibonachi(3));
+   }
+}
