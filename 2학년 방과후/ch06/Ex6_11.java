30161a267538cc8e008fe44eee574ac7cfb4200a
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-06-27 17:43:24 +0900

    Create Ex6_11.java

---
+++ 2학년 방과후/ch06/Ex6_11.java
@@ -0,0 +1,22 @@
+package ch06;
+
+class Data1 {
+	int value;
+}
+
+class Data22 {
+	int value;
+	Data22() {}
+	Data22(int x) {
+		value = x;
+	}
+}
+
+public class Ex6_11 {
+
+	public static void main(String[] args) {
+		Data1 d1 = new Data1();
+		Data22 d2 = new Data22();
+	}
+
+}
