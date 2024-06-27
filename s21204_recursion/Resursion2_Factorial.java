9b8e72168e5e8b14b25b17dfcc02a79b4631f80e
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-06-27 10:42:14 +0900

    Create Resursion2_Factorial.java

---
+++ s21204_recursion/Resursion2_Factorial.java
@@ -0,0 +1,17 @@
+package s21204_recursion;
+
+public class Resursion2_Factorial {
+	static int factorial(int num) {
+		System.out.println("ing : " +num);
+		if (num == 1) {
+			return 1;
+		} else {
+			return num*factorial(num-1);
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Factorial : " +factorial(5));
+	}
+
+}
