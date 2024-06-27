5b1154332b7a77143ab9c1fd88e1871b4079e21f
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-06-27 10:41:56 +0900

    Create Recursion1_SumPlus.java

---
+++ s21204_recursion/Recursion1_SumPlus.java
@@ -0,0 +1,18 @@
+package s21204_recursion;
+
+public class Recursion1_SumPlus {
+	
+	static int sumPlus(int num) {
+		System.out.println("ing : " +num);
+		if(num == 1) {
+			return 1;
+		} else {
+			return num+sumPlus(num-1);
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Sum : " +sumPlus(10));
+	}
+
+}
