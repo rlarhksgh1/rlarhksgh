bdd9f6bb65616b8b42ff0a7fb57251832e27883b
rlarhksgh1 <128556355+rlarhksgh1@users.noreply.github.com>
2024-06-27 10:32:27 +0900

    Create Chap01.java

---
+++ s21204_java10/Chap01.java
@@ -0,0 +1,26 @@
+package s21204_java10;
+
+import s21204_java11.Matiz;
+
+public class Chap01 {
+
+	public static void main(String[] args) {
+
+		Animal am = new Animal();
+		am.a();
+		
+		Dog d = new Dog();
+		d.a();
+		
+		Fish f = new Fish();
+		f.a();
+		
+		Bird bd = new Bird();
+		bd.a();
+		
+		Matiz m = new Matiz();
+		m.a();	
+
+	}
+
+}
